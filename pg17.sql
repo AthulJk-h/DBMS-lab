a9161d3bf93a7a3552ff99e92b8eeb4ca7959f76
AthulJk-h <76027722+AthulJk-h@users.noreply.github.com>
2022-05-31 11:13:54 +0530

    Update pg17.sql

--- pg17.sql
+++ pg17.sql
@@ -2,7 +2,7 @@ SQL> CREATE OR REPLACE TRIGGER display_salary_changes
   2  BEFORE DELETE OR INSERT OR UPDATE ON empl
   3  FOR EACH ROW
   4  WHEN (NEW.ID > 0)
-  5  DECLARE
+  5  DECLAREhttps://github.com/AthulJk-h/DBMS-lab/blob/main/pg17.sql
   6  sal_diff number;
   7  BEGIN
   8  sal_diff := :NEW.salary - :OLD.salary;
@@ -15,19 +15,21 @@ SQL> CREATE OR REPLACE TRIGGER display_salary_changes
 Trigger created.
 ****************************
 
-SQL> CREATE OR REPLACE TRIGGER display_salary_changes
-  2   BEFORE DELETE OR INSERT OR UPDATE ON empl
-  3  FOR EACH ROW
-  4  WHEN (NEW.ID > 0)
-  5  DECLARE
-  6  sal_diff number;
-  7  BEGIN
-  8   sal_diff := :NEW.salary - :OLD.salary;
-  9  dbms_output.put_line('Old salary: ' || :OLD.salary);
- 10  dbms_output.put_line('New salary: ' || :NEW.salary);
- 11  dbms_output.put_line('Salary difference: ' || sal_diff);
- 12   END;
- 13  /
+SQL> set serverout on
+SQL> DECLARE
+  2  total_rows number(2);
+  3  BEGIN
+  4  UPDATE empl
+  5  SET salary = salary + 5000;
+  6  IF sql%notfound THEN
+  7  dbms_output.put_line('no customers updated');
+  8  ELSIF sql%found THEN
+  9  total_rows := sql%rowcount;
+ 10  dbms_output.put_line( total_rows || ' customers updated ');
+ 11  END IF;
+ 12
+ 13  END;
+ 14  /
 Old salary: 25000
 New salary: 30000
 Salary difference: 5000
